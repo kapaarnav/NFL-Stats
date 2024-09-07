58258f14a969efbe8289e943b77306aa4a6db53d
Aarnav Kapoor <kapaarnav@gmail.com>
2024-09-07 03:51:21 -0400

    parse alphabet link

--- scrape.py
+++ scrape.py
@@ -1,1 +1,13 @@
-print("hello world")
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = "https://www.pro-football-reference.com/players/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="content")
+job_elements = results.find("ul", class_="page_index")
+
+for job_element in job_elements:
+    print ("Found the URL:", job_element.find('a', href=True)['href'])
