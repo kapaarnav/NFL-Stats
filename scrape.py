e4300d8834a9a08526815c17984e28c729d1b059
dxyu05 <dyu12@terpmail.umd.edu>
2024-09-16 00:55:58 -0400

    add venv config and gitignore

--- scrape.py
+++ scrape.py
@@ -22,25 +22,28 @@ results = soup.find(id="content")
 items = results.find("ul", class_="page_index")
 
 
-#Loops through each list item and builds a link to a letter
-for item in items:
+# #Loops through each list item and builds a link to a letter
+# for item in items:
     
-    #Gets the link to each letter and parses through it
-    link = "https://www.pro-football-reference.com" + str(item.find('a', href=True)['href'])
-    page = requests.get(link)
-    soup = BeautifulSoup(page.content, "html.parser")
+#     #Gets the link to each letter and parses through it
+#     link = "https://www.pro-football-reference.com" + str(item.find('a', href=True)['href'])
+#     page = requests.get(link)
+#     soup = BeautifulSoup(page.content, "html.parser")
 
-    #Gets the link of each player that is currently active
-    players = soup.find(id="div_players")
-    for bold in players.find_all('b'):
-        link = "https://www.pro-football-reference.com"  + str(bold.find('a', href = True)['href'])
-        print(link)
+#     #Gets the link of each player that is currently active
+#     players = soup.find(id="div_players")
+#     for bold in players.find_all('b'):
+#         link = "https://www.pro-football-reference.com"  + str(bold.find('a', href = True)['href'])
+#         print(link)
         
-    #Sleeps to avoid rate limiting as per PFR scraping guidlines
-    time.sleep(4)
+#     #Sleeps to avoid rate limiting as per PFR scraping guidlines
+#     time.sleep(4)
 
 #Gets the table, apply below
 url = requests.get('https://www.pro-football-reference.com/players/E/EricAl01.htm')
+soup = BeautifulSoup(url.content, 'html.parser')
+position = (soup.findAll('p')[1].text)[0:14] #substring to get position
+print(position)
 c = url.content
 df = pd.read_html(c)[0]
-print(df)
+#print(df)
